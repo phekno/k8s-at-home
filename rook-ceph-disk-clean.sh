49316110f0fd5559dc26288c7f5039bb10be7a0e
Joshua Fechner <phekno@gmail.com>
2025-01-29 14:31:30 +0000

    adds rook-ceph back

--- rook-ceph-disk-clean.sh
+++ rook-ceph-disk-clean.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 kubectl apply -f disk-clean.yaml
-kubectl apply -f disk-wipe.yaml
+kubectl apply -f disk-zap.yaml
 kubectl delete pods disk-clean-0 disk-clean-1 disk-clean-2
-kubectl delete pods disk-wipe-0 disk-wipe-1 disk-wipe-2
+kubectl delete pods disk-zap-0 disk-zap-1 disk-zap-2
